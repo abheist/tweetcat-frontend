773c82ce24bbc5da0a3e749c8cd9e2cc8abfae38
abheist <abhishek291093@gmail.com>
2023-03-08 15:41:41 -0400

    🌼payments | stripe | added customer portal button and backend integration, by which user can go to customer portal and can manage their subscription.

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,18 +1,42 @@
 import {useRouter} from "next/router";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {post} from "@/utils/fetchMiddleware";
+import {useMutation} from "react-query";
+
+export const customerPortal = (sessionId: string) => {
+    return post('payments/customer-portal-url/', {sessionId: sessionId})
+}
+
 
 export default function Success() {
+    const [sessionId, setSessionId] = useState(() => '')
     const router = useRouter()
+
+    const mutation = useMutation(customerPortal)
+
 
     // TODO: send the session_id to backend for further processing
     useEffect(() => {
-        const {session_id} = router.query
-        if (session_id) {
-            console.log(session_id)
+        const {session_id: sessionId}: any = router.query
+        if (sessionId) {
+            setSessionId(sessionId)
         }
     }, [router])
 
+    const handleClick = async (data: any) => {
+        const response = await mutation.mutateAsync(sessionId)
+        const result = await response.json()
+        if (result.redirect) {
+            window.location = result.redirect;
+        }
+    }
+
+
     return (
-        <div className={`text-white`}>Your payment was successfull!</div>
+        <div>
+            <div className={`text-white`}>Your payment was successfull!</div>
+            {/*TODO: Move this customer portal button in settings or to navigation bar, so everyone can see it.*/}
+            <button className={`btn`} onClick={handleClick}>Customer Portal</button>
+        </div>
     )
 }
