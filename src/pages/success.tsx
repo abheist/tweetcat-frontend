850c4458b426e27787b2471c729864c89151cc86
abheist <abhishek291093@gmail.com>
2023-03-08 15:09:59 -0400

    🌼payments: sucess page handling session_id which we are getting back from stripe

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,18 @@
+import {useRouter} from "next/router";
+import {useEffect} from "react";
+
 export default function Success() {
+    const router = useRouter()
+
+    // TODO: send the session_id to backend for further processing
+    useEffect(() => {
+        const {session_id} = router.query
+        if (session_id) {
+            console.log(session_id)
+        }
+    }, [router])
+
     return (
-        <div>Your payment was successfull!</div>
+        <div className={`text-white`}>Your payment was successfull!</div>
     )
 }
