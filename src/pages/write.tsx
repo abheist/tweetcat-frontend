b31ca5c4357e78761ab4622f192b4ebf522a9415
abheist <abhishek291093@gmail.com>
2023-03-18 18:26:22 -0300

    ✨write | added button to dashboard

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -68,6 +68,7 @@ const Write = () => {
             <div className={`flex-grow`}>
                 <div className="navbar">
                     <div className={`flex-grow flex justify-end gap-x-2`}>
+                        <button className="btn btn-ghost btn-xs text-primary">Dashboard</button>
                         <button className="btn btn-outline btn-xs btn-primary">Schedule</button>
                         <button className="btn btn-outline btn-xs btn-primary">Tweet Now</button>
                     </div>
