cc49882ccc66a468a79c3c2e20ee61f075606ad4
abheist <abhishek291093@gmail.com>
2023-03-18 20:15:18 -0300

    ✨sidebar | created sidebar.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import {ReactNode} from "react";
 import {axiosPrivate} from "@/utils/axiosPrivate";
 import {Header} from "@/components/header";
+import Sidebar from "@/components/sidebar";
 
 interface LayoutProps {
     children: ReactNode
@@ -14,6 +15,7 @@ export default function Layout({children}: LayoutProps) {
 
     return (
         <>
+            <Sidebar />
             <div className={`container mx-auto lg:px-48`}><Header/></div>
             <div className={`py-16`}>
                 {children}
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,50 @@
+import {FiCalendar, FiFeather, FiHome, FiTrendingUp} from "react-icons/fi";
+
+function SideBarAppIcon({icon, tooltip}: any) {
+    return (
+        <div className="tooltip tooltip-right" data-tip={tooltip}>
+            <button className="btn btn-ghost">
+                {icon}
+            </button>
+        </div>
+    );
+}
+
+export default function Sidebar() {
+    return (
+        <div className={`flex w-16 border-r min-h-screen bg-blue-100 py-4`}>
+            <div className={`flex-grow flex flex-col items-center`}>
+                <SideBarAppIcon icon={"N"} tooltip={`Niyam`}/>
+
+                <div className={`flex-grow`}></div>
+
+                <SideBarAppIcon icon={<FiHome/>} tooltip={`Home`}/>
+                <SideBarAppIcon icon={<FiFeather/>} tooltip={`Tweet`}/>
+                <SideBarAppIcon icon={<FiCalendar/>} tooltip={`Calendar`}/>
+                <SideBarAppIcon icon={<FiTrendingUp/>} tooltip={`Analytics`}/>
+
+                <div className={`flex-grow`}></div>
+
+                <div className="dropdown dropdown-end tooltip tooltip-right" data-tip="Settings">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full ring">
+                            <img
+                                src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80"/>
+                        </div>
+                    </label>
+                    <ul tabIndex={0}
+                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
