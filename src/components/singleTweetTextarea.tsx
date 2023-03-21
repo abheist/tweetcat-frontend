4f7d60da9ed88b4f9d9f8e6089b3ec1dbc766c4f
abheist <abhishek291093@gmail.com>
2023-03-21 00:03:39 -0300

    ✨tweet page | added icons to tweet and show/hide based on hover

--- src/components/singleTweetTextarea.tsx
+++ src/components/singleTweetTextarea.tsx
@@ -1,17 +1,24 @@
 import ContentEditable from "react-contenteditable";
 import React from "react";
+import {FiImage, FiPlus, FiX} from "react-icons/fi";
+import {RxDragHandleDots2} from "react-icons/rx";
 
 export function SingleTweetTextarea(props: { onChange: (evt: any) => void, html: string }) {
-    return <div className={`flex gap-x-2`}>
-        <div>
-            <div className="avatar">
-                <div className="w-8 rounded-full">
-                    <img
-                        src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80"/>
+    return <div className={`flex gap-x-2 group/main`}>
+        <div className={`flex`}>
+            <button className={`btn btn-square btn-sm btn-ghost cursor-grab active:cursor-grabbing`}>
+                <RxDragHandleDots2/>
+            </button>
+            <div>
+                <div className="avatar">
+                    <div className="w-8 rounded-full">
+                        <img
+                            src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80"/>
+                    </div>
                 </div>
             </div>
         </div>
-        <div>
+        <div class={``}>
             <div className={`flex items-center gap-x-2`}>
                 <span className={`font-bold`}>Abhishek Singh</span>
                 <span className={`text-slate-400 text-sm`}>- @abheist</span>
@@ -21,26 +28,28 @@ export function SingleTweetTextarea(props: { onChange: (evt: any) => void, html:
                 onChange={props.onChange}
                 onBlur={props.onChange}
                 html={props.html}/>
-            <div className={`flex gap-x-2`}>
+            <div className={`flex gap-x-2 h-8 items-center`}>
                 <div className={`flex-grow flex flex-col justify-center`}>
                     <div className={`h-[0.05px] bg-slate-300`}></div>
                 </div>
-                <div className="">
-                    <div className="radial-progress bg-slate-100 border-2 border-slate-100" style={{
-                        "--size": "1rem",
-                        "--value": 70
-                    }}/>
-                    <button className="btn btn-square btn-ghost btn-xs">
-                        A
+                <div className="flex justify-between items-center gap-x-1 hidden group-hover/main:flex">
+                    <button className={`btn btn-xs btn-outline border-slate-200`}>
+                        <div className="radial-progress" style={{
+                            "--size": "0.8rem",
+                            "--value": 70
+                        }}/>
                     </button>
-                    <button className="btn btn-square btn-ghost btn-xs">
-                        B
+                    <button className="btn btn-square btn-outline border-slate-200 btn-xs font-light">
+                        #2
                     </button>
-                    <button className="btn btn-square btn-ghost btn-xs">
-                        C
+                    <button className="btn btn-square btn-outline border-slate-200 btn-xs">
+                        <FiImage/>
                     </button>
-                    <button className="btn btn-square btn-ghost btn-xs">
-                        D
+                    <button className="btn btn-square btn-outline border-slate-200 btn-xs">
+                        <FiPlus/>
+                    </button>
+                    <button className="btn btn-square btn-outline border-slate-200 btn-xs">
+                        <FiX/>
                     </button>
                 </div>
             </div>
--- src/components/tweetInputArea.tsx
+++ src/components/tweetInputArea.tsx
@@ -2,7 +2,7 @@ import {SingleTweetTextarea} from "@/components/singleTweetTextarea";
 
 export function TweetInputArea(props: { onChange: (evt: any) => void, html: string }) {
     return <div className={`flex-grow flex justify-center items-center mt-16`}>
-        <div>
+        <div className={`space-y-4`}>
             <SingleTweetTextarea onChange={props.onChange} html={props.html}/>
             <SingleTweetTextarea onChange={props.onChange} html={props.html}/>
             <SingleTweetTextarea onChange={props.onChange} html={props.html}/>
