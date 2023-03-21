74ca8572a96d33996257844030f61a73a47ef6de
abheist <abhishek291093@gmail.com>
2023-03-21 20:31:15 -0300

    ✨tweet page | schedule sidebar | added search bar for human language

--- src/components/scheduleSidebar.tsx
+++ src/components/scheduleSidebar.tsx
@@ -2,8 +2,12 @@ import {ScheduleEvent} from "@/components/scheduleEvent";
 
 export function ScheduleSidebar() {
     return (
-        <div className="">
+        <div className="space-y-4">
             <h2 className="card-title">Schedule</h2>
+            <div>
+                <input type="text" placeholder="Type time here in natural language"
+                       className="input input-bordered input-sm w-full max-w-xs"/>
+            </div>
             <div>
                 <div className={`sticky top-0 z-10 bg-white py-4 bg-opacity-95`}>
                     <p className={`text-xs font-bold`}>21 March</p>
