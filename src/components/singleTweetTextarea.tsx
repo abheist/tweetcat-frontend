26e0c9db1df81afd399d8806f35a1f8ad5c86d49
abheist <abhishek291093@gmail.com>
2023-03-23 22:53:52 -0300

    ✨tweet area | user can multiple tweet, handles the plus button

--- src/components/singleTweetTextarea.tsx
+++ src/components/singleTweetTextarea.tsx
@@ -1,9 +1,26 @@
 import ContentEditable from "react-contenteditable";
-import React from "react";
 import {FiImage, FiPlus, FiX} from "react-icons/fi";
 import {RxDragHandleDots2} from "react-icons/rx";
+import React from "react";
+import sanitizeHtml from "sanitize-html";
 
-export function SingleTweetTextarea(props: { onChange: (evt: any) => void, html: string }) {
+interface SingleTweetTextareaProps {
+    content: string,
+    onChange: (evt: any) => void
+    addTweet: (evt: any) => void
+}
+
+export function SingleTweetTextarea({content, onChange, addTweet}: any) {
+    const [tweet, setTweet] = React.useState(() => content);
+
+    const onContentChange = React.useCallback((evt: any) => {
+        const sanitizeConf = {};
+        setTweet(sanitizeHtml(evt.currentTarget.innerHTML, sanitizeConf))
+    }, [])
+
+    const progressValues = {"--size": "0.8rem", "--value": 70}
+
+
     return <div className={`flex gap-x-2 group/main`}>
         <div className={`flex`}>
             <button className={`btn btn-square btn-sm btn-ghost cursor-grab active:cursor-grabbing`}>
@@ -18,26 +35,24 @@ export function SingleTweetTextarea(props: { onChange: (evt: any) => void, html:
                 </div>
             </div>
         </div>
-        <div class={``}>
+        <div>
             <div className={`flex items-center gap-x-2`}>
                 <span className={`font-bold`}>Abhishek Singh</span>
                 <span className={`text-slate-400 text-sm`}>- @abheist</span>
             </div>
             <ContentEditable
                 className={`w-96 px-4 py-2`}
-                onChange={props.onChange}
-                onBlur={props.onChange}
-                html={props.html}/>
+                onChange={onContentChange}
+                onBlur={onContentChange}
+                html={tweet}/>
             <div className={`flex gap-x-2 h-8 items-center`}>
                 <div className={`flex-grow flex flex-col justify-center`}>
                     <div className={`h-[0.05px] bg-slate-300`}></div>
                 </div>
                 <div className="flex justify-between items-center gap-x-1 hidden group-hover/main:flex">
                     <button className={`btn btn-xs btn-outline border-slate-200`}>
-                        <div className="radial-progress" style={{
-                            "--size": "0.8rem",
-                            "--value": 70
-                        }}/>
+                        {/*@ts-ignore*/}
+                        <div className="radial-progress" style={progressValues}/>
                     </button>
                     <button className="btn btn-square btn-outline border-slate-200 btn-xs font-light">
                         #2
@@ -45,7 +60,7 @@ export function SingleTweetTextarea(props: { onChange: (evt: any) => void, html:
                     <button className="btn btn-square btn-outline border-slate-200 btn-xs">
                         <FiImage/>
                     </button>
-                    <button className="btn btn-square btn-outline border-slate-200 btn-xs">
+                    <button className="btn btn-square btn-outline border-slate-200 btn-xs" onClick={addTweet}>
                         <FiPlus/>
                     </button>
                     <button className="btn btn-square btn-outline border-slate-200 btn-xs">
@@ -53,7 +68,6 @@ export function SingleTweetTextarea(props: { onChange: (evt: any) => void, html:
                     </button>
                 </div>
             </div>
-            {/*<textarea className={`w-96 px-4 py-2`} placeholder={`What's happening?`} maxLength={120} />*/}
         </div>
     </div>;
 }
--- src/components/tweetInputArea.tsx
+++ src/components/tweetInputArea.tsx
@@ -1,14 +1,26 @@
 import {SingleTweetTextarea} from "@/components/singleTweetTextarea";
+import {useState} from "react";
+import sanitizeHtml from "sanitize-html";
 
-export function TweetInputArea(props: { onChange: (evt: any) => void, html: string }) {
-    return <div className={`flex-grow flex justify-center items-center mt-16`}>
+interface tweetsProps {
+    content: string,
+    seq: number
+    id: number
+}
+
+export function TweetInputArea() {
+    const [tweets, setTweets] = useState<tweetsProps[]>(() => [{content: "", seq: 0, id: Math.random()}]);
+    const sanitizeConf = {};
+
+    const addTweet = (evt: any) => {
+        const newContent = sanitizeHtml(evt.target.value, sanitizeConf)
+        const newState = [...tweets, {id: Math.random(), content: newContent, seq: tweets.length}]
+        setTweets(newState)
+    }
+
+    return <div className={`flex-grow flex justify-center items-center mt-16 pb-56`}>
         <div className={`space-y-4`}>
-            <SingleTweetTextarea onChange={props.onChange} html={props.html}/>
-            <SingleTweetTextarea onChange={props.onChange} html={props.html}/>
-            <SingleTweetTextarea onChange={props.onChange} html={props.html}/>
-            <SingleTweetTextarea onChange={props.onChange} html={props.html}/>
-            <SingleTweetTextarea onChange={props.onChange} html={props.html}/>
-            <SingleTweetTextarea onChange={props.onChange} html={props.html}/>
+            {tweets.map((tweet) => <SingleTweetTextarea key={tweet.id} addTweet={addTweet} content={tweet.content}/>)}
         </div>
     </div>;
 }
