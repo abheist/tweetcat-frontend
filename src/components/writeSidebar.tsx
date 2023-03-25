14b68fae7d1b15b1ec5990fcea478421906c210c
abheist <abhishek291093@gmail.com>
2023-03-25 19:39:17 -0300

    ✨tweet area | made sidebar dynamic

--- src/components/writeSidebar.tsx
+++ src/components/writeSidebar.tsx
@@ -1,74 +1,77 @@
+import {useEffect, useState} from "react";
+import {TweetItem} from "@/components/tweetItem";
+
+interface SideBarTweetsProps {
+    text: string
+}
+
 export function WriteSidebar() {
+    const [tweets, setTweets] = useState<SideBarTweetsProps[]>(() => []);
+    const tabs = [`Drafts`, `Scheduled`, `Published`];
+    const [tab, setTab] = useState(() => tabs[0]);
+    const [selectedTweet, setSelectedTweet] = useState(() => 0);
+
+    const drafts = [
+        {text: `What is happening here, you are not the controller of it.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen. Woha woha woha`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+    ]
+
+    const scheduled = [
+        {text: `Tabs 2 What is happening here, you are not the controller of it.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen. Woha woha woha`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+    ]
+
+    const published = [
+        {text: ` Tabs 3 What is happening here, you are not the controller of it.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen. Woha woha woha`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+        {text: `What is happening here, you are not the controller of it, you can just do what you want and it will happen.`},
+    ]
+
+    useEffect(() => {
+        setTweets(() => {
+            switch (tab) {
+                case `Drafts`:
+                    return [...drafts];
+                case `Scheduled`:
+                    return [...scheduled];
+                case `Published`:
+                    return [...published];
+                default:
+                    return [];
+            }
+        })
+    }, [tab])
+
+
     return (
         <div className={`w-80 bg-base-200 border-r h-screen flex flex-col`}>
             <div className="tabs tabs-boxed flex justify-around my-4">
-                <a className="tab tab-md tab-active">Drafts</a>
-                <a className="tab tab-md">Scheduled</a>
-                <a className="tab tab-md">Published</a>
+                {tabs.map((tabName) =>
+                    <a key={tabName}
+                       onClick={() => setTab(tabName)}
+                       className={`tab tab-md ${tabName === tab ? 'tab-active' : ''}`}>
+                        {tabName}
+                    </a>
+                )}
             </div>
             <ul className="w-full text-sm divide-y border-t border-b overflow-y-auto flex-grow">
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4 bg-white border-l-4 border-l-primary group relative`}>
-                    <div
-                        className={`absolute py-2 px-4 right-0 flex gap-x-2 bg-gradient-to-l from-white to-white/80 
-                                transition-opacity duration-500 ease-in-out
-                                opacity-0  group-hover:opacity-100`}>
-                        <button className="btn btn-xs btn-square btn-outline">
-                            E
-                        </button>
-                    </div>
-                    <p className={``}>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
-                <li className={`px-6 py-4`}>
-                    <p>What is happening here, you are not the controller of it, you can just...</p>
-                </li>
+                {tweets.map((tweet, index) =>
+                    <TweetItem active={index === selectedTweet}
+                               onClick={() => setSelectedTweet(index)}
+                               key={index}
+                               text={tweet.text}/>
+                )}
             </ul>
         </div>);
 }
