a4887322d3bddd113d1cb418a6e052640acc071b
abheist <abhishek291093@gmail.com>
2023-04-05 23:49:45 -0300

    intermediate refactor

--- src/components/tweetTable.tsx
+++ src/components/tweetTable.tsx
@@ -4,6 +4,76 @@ interface TweetTableProps {
     tweets: any[]
 }
 
+function NoTweetPlaceholder({tweets}: TweetTableProps) {
+    return (
+        <div>
+            <div className="overflow-x-auto relative rounded">
+                <div
+                    className={`absolute bg-gray-100/95 z-20 w-full h-full flex justify-center items-center`}>
+                    <div className={`space-y-4`}>
+                        <p className={`text-lg mx-auto font-bold italic font-serif text-gray-500 text-center w-2/3`}>
+                            Please upload a excel or csv file above with the below format. Your tweets will appear here.
+                        </p>
+                        <div className="overflow-x-auto">
+                            <table className="table table-compact w-full">
+                                {/*TODO: add a sample file here*/}
+                                <caption className="caption-bottom mt-2">
+                                    You can download a sample file from <a className={`text-primary`} href="#">here</a>.
+                                </caption>
+                                <thead>
+                                <tr>
+                                    <th className={`lowercase`}>tweet_text</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>I am not what happened to me, I am what I choose to become.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Knowing your own darkness is the best method for dealing with the darknesses of
+                                        other people.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>The privilege of a lifetime is to become who you truly are.</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <table className="table table-auto table-compact w-full relative">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Tweet</th>
+                        <th>Last published</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        tweets.map((tweet: any) => (
+                            <tr key={tweet.id}>
+                                <td>{tweet.id}</td>
+                                <td className={`line-clamp-1`}>{tweet.tweetText}</td>
+                                <td>{tweet.lastPublishedDate}</td>
+                            </tr>
+                        ))
+                    }
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <th></th>
+                        <th>Tweet</th>
+                        <th>Last Published</th>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>);
+}
+
 export function TweetTable({tweets = []}: TweetTableProps) {
 
 
@@ -56,72 +126,7 @@ export function TweetTable({tweets = []}: TweetTableProps) {
     ]
 
     if (tweets?.length === 0) return (
-        <div>
-            <div className="overflow-x-auto relative rounded">
-                <div
-                    className={`absolute bg-gray-100/95 z-20 w-full h-full flex justify-center items-center`}>
-                    <div className={`space-y-4`}>
-                        <p className={`text-lg mx-auto font-bold italic font-serif text-gray-500 text-center w-2/3`}>
-                            Please upload a excel or csv file above with the below format. Your tweets will appear here.
-                        </p>
-                        <div className="overflow-x-auto">
-                            <table className="table table-compact w-full">
-                                {/*TODO: add a sample file here*/}
-                                <caption className="caption-bottom mt-2">
-                                    You can download a sample file from <a className={`text-primary`} href="#">here</a>.
-                                </caption>
-                                <thead>
-                                <tr>
-                                    <th className={`lowercase`}>tweet_text</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>I am not what happened to me, I am what I choose to become.
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Knowing your own darkness is the best method for dealing with the darknesses of
-                                        other people.
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>The privilege of a lifetime is to become who you truly are.</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                <table className="table table-auto table-compact w-full relative">
-                    <thead>
-                    <tr>
-                        <th></th>
-                        <th>Tweet</th>
-                        <th>Last published</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        mockTweets.map((tweet) => (
-                            <tr key={tweet.id}>
-                                <td>{tweet.id}</td>
-                                <td className={`line-clamp-1`}>{tweet.tweetText}</td>
-                                <td>{tweet.lastPublishedDate}</td>
-                            </tr>
-                        ))
-                    }
-                    </tbody>
-                    <tfoot>
-                    <tr>
-                        <th></th>
-                        <th>Tweet</th>
-                        <th>Last Published</th>
-                    </tr>
-                    </tfoot>
-                </table>
-            </div>
-        </div>
+        <NoTweetPlaceholder tweets={mockTweets}/>
     )
 
     return <div className="overflow-x-auto">
