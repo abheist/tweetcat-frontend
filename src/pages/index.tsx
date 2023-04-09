0762f4404642908919e746712e5d6db74a835f29
abheist <abhishek291093@gmail.com>
2023-04-08 23:19:38 -0300

    😎 | index page | added the third section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -116,6 +116,28 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
+                    <div className={`container mx-auto min-h-screen pt-24`}>
+                        <div className={`flex flex-col items-center gap-y-6`}>
+                            <h1 className={`text-7xl font-black text-center w-4/5 leading-snug tracking-wide`}>
+                                Designed for Solopreneurs, Businesses, and Agencies
+                            </h1>
+                            <div className={`relative w-11/12 h-48 mt-8`}>
+                                <Image src="/tweetcat_analytics.png" alt="TweetCat analytics" fill
+                                       style={{objectFit: "contain", objectPosition: "center"}}/>
+                            </div>
+                            <p className={`text-4xl font-medium text-center w-3/5 leading-relaxed`}>
+                                Focus on your passion, focus on the thing, focus on your destiny, call and be the
+                                builder
+                            </p>
+                            <button
+                                className={`btn btn-lg bg-blue-500 rounded-full font-extrabold text-white mt-12 
+                                            border-blue-500 shadow-lg shadow-blue-500 btn-wide
+                                            bg-gradient-to-b from-blue-500 to-blue-700`}
+                                onClick={() => handleLoginWithTwitter()}>
+                                BE THE BUILDER
+                            </button>
+                        </div>
+                    </div>
                 </Layout>
             </main>
         </>
