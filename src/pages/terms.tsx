be22e023335516bd7ce37147d91dfb657fe05409
abheist <abhishek291093@gmail.com>
2023-04-11 23:53:51 -0300

    😎 | terms.tsx | added

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -15,9 +15,6 @@ const TermsPage = () => {
                             <Link href={`/`} className={`text-gray-500 hover:text-gray-700`}>
                                 Home
                             </Link>
-                            <Link href={`/terms`} className={`text-gray-500 hover:text-gray-700`}>
-                                Terms
-                            </Link>
                         </div>
                     </div>
                     <div className={`space-y-4`}>
